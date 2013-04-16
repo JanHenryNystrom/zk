61cf57b80dd994774bce1d4f1903547787604862
Henry Nystrom <JanHenryNystrom@gmail.com>
2013-04-16 09:59:01 +0200

    Added the collection of names for the analysis stage.

--- src/zk_hadoop_record.erl
+++ src/zk_hadoop_record.erl
@@ -29,6 +29,7 @@
 -export([compile/1, compile/2]).
 
 %% Includes
+-include_lib("zk/src/zk_hadoop_record.hrl").
 
 %% Records
 -record(opts, {dest_name :: string(),
@@ -65,26 +66,69 @@ compile(File) -> compile(File, []).
 -spec compile(atom() | string(), [opt()]) -> ok | error.
 %%--------------------------------------------------------------------
 compile(Atom, Opts) when is_atom(Atom) -> compile(atom_to_list(Atom), Opts);
-compile(File, Opts) ->
-    OptsRec = #opts{src_dir = Dir} = parse_opts(Opts, #opts{}),
-    FileName = case filename:extension(File) of
-                   [] -> filename:join(Dir, File ++ ".jute");
-                   ".jute" -> filename:join(Dir, File)
-               end,
-    case zk_hadoop_record_parse:file(FileName) of
-        {ok, Tree} -> Tree;
-        {error, {Line, Module, Message}} ->
-            format_error(Module, Message, Line),
-            error
-    end.
+compile(File, Opts) -> do_compile(File, parse_opts(Opts, #opts{})).
 
 %% ===================================================================
 %% Internal functions.
 %% ===================================================================
 
+do_compile(File, Opts) ->
+    chain({ok, File}, Opts, [fun parse/2, fun analyse/2]).
+
+chain(Result, _, []) -> Result;
+chain({ok, Previous}, Opts, [Fun | T]) -> chain(Fun(Previous, Opts), Opts, T);
+chain(error, _, _) -> error.
+
+parse(File, #opts{src_dir = Dir}) ->
+    FileName = case filename:extension(File) of
+                   [] -> filename:join(Dir, File ++ ".jute");
+                   ".jute" -> filename:join(Dir, File)
+               end,
+    case zk_hadoop_record_parse:file(FileName) of
+        {ok, Tree} -> {ok, Tree};
+        {error, {Line, Module, Message}} ->
+            format_error(Module, Message, Line),
+            error
+    end.
+
+analyse(Tree, Opts) -> analyse(Tree, dict:new(), Opts).
+
+analyse(#file{includes = [], modules = Modules}, Names, _) ->
+    Names1 = lists:foldl(fun analyse_module/2, Names, Modules),
+    Names1.
+
+analyse_module(#module{name = Name, records = Recs}, Names) ->
+    Value = value(Name),
+    case dict:is_key(Value, Names) of
+        true ->
+            exit({duplicate_name, Value, line(Name)});
+        false ->
+            Names0 = dict:store(module, Name, dict:store(Value,[module],Names)),
+            Names1 = lists:foldl(fun analyse_record/2, Names0, Recs),
+            dict:erase(module, Names1)
+    end.
+
+analyse_record(#record{name = Name, line = Line}, Names) ->
+    Module = dict:fetch(module, Names),
+    FullName = fullname(Module, Name),
+    case dict:is_key(FullName, Names) of
+        true ->
+            exit({duplicate_name, Name, Line});
+        false ->
+            dict:store(FullName, [record], Names)
+    end.
+
+line(#name{line = Line}) -> Line.
+
+value(#name{value = Value}) -> Value.
+
+fullname(#name{type = simple, value  = Value1}, #name{value = Value2}) ->
+    [Value1, Value2];
+fullname(#name{value  = Value1}, #name{value = Value2}) ->
+    Value1 ++ [Value2].
+
 format_error(Module, Message, Line) ->
     io:format("Error Line ~p:~s~n", [Line, Module:format_error(Message)]).
-
 
 parse_opts([], Rec) -> Rec;
 parse_opts(Opts, Rec) -> lists:foldl(fun parse_opt/2, Rec, Opts).
